c571ebe67ebe6e6c44a6ac77ee3c87279fd429a5
alxrw <alxr.wagner@gmail.com>
2023-09-09 10:58:59 +0700

    completed

--- src/main/java/com.example.playtox/model/Account.java
+++ src/main/java/com.example.playtox/model/Account.java
@@ -1,10 +1,16 @@
 package com.example.playtox.model;
 
+import com.example.playtox.exception.NotEnoughMoneyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.TransferService;
+
 import java.util.UUID;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Account {
+    private final Logger logger = LoggerFactory.getLogger(TransferService.class);
     private final ReadWriteLock lockAccount;
     private final String ID;
     private int money;
@@ -40,15 +46,20 @@ public class Account {
         }
     }
 
-    public void withdrawMoney(int moneyAmount){
-        if (moneyAmount <= 0){
-            throw new IllegalArgumentException("Transfer amount must be greater than 0");
-        }
-        lockAccount.writeLock().lock();
-        try {
+    public void withdrawMoney(int moneyAmount) {
+        if (money >= moneyAmount) {
+            if (moneyAmount <= 0) {
+                throw new IllegalArgumentException("Transfer amount must be greater than 0");
+            }
+            lockAccount.writeLock().lock();
+            try {
                 this.money -= moneyAmount;
-        }finally {
-            lockAccount.writeLock().unlock();
+            } finally {
+                lockAccount.writeLock().unlock();
+            }
+        } else {
+            logger.info("Not enough money in the account {}", ID);
+            throw new NotEnoughMoneyException("Not enough money in the account " + ID);
         }
     }
 }
--- src/main/java/service/TransferService.java
+++ src/main/java/service/TransferService.java
@@ -1,6 +1,5 @@
 package service;
 
-import com.example.playtox.exception.NotEnoughMoneyException;
 import com.example.playtox.exception.NotFoundAccount;
 import com.example.playtox.model.Account;
 import org.slf4j.Logger;
@@ -20,18 +19,13 @@ public class TransferService {
         logger.info("A transfer operation has been started. From ID: {} | To ID: {}", accountIDFrom, accountIDTo);
 
         Account accountFrom = accountRepository.findAccountByID(accountIDFrom).orElseThrow(NotFoundAccount::new);
-        Account accountTo = accountRepository.findAccountByID(accountIDFrom).orElseThrow(NotFoundAccount::new);
+        Account accountTo = accountRepository.findAccountByID(accountIDTo).orElseThrow(NotFoundAccount::new);
 
-        if (accountFrom.getMoney() >= moneyAmount) {
-            accountFrom.withdrawMoney(moneyAmount);
-            logger.info("The amount of money {} was withdraw from ID: {}", moneyAmount, accountIDFrom);
-            accountTo.addMoney(moneyAmount);
-            logger.info("The amount of money {} was add to ID: {}", moneyAmount, accountIDTo);
-            logger.info("Account ID: {} | account balance is {}", accountIDFrom, accountFrom.getMoney());
-            logger.info("Account ID: {} | account balance is {}", accountIDTo, accountTo.getMoney());
-
-        } else {
-            throw new NotEnoughMoneyException("Not enough money in the account " + accountIDFrom);
-        }
+        accountFrom.withdrawMoney(moneyAmount);
+        logger.info("The amount of money {} was withdraw from ID: {}", moneyAmount, accountIDFrom);
+        accountTo.addMoney(moneyAmount);
+        logger.info("The amount of money {} was add to ID: {}", moneyAmount, accountIDTo);
+        logger.info("Account ID: {} | account balance is {}", accountIDFrom, accountFrom.getMoney());
+        logger.info("Account ID: {} | account balance is {}", accountIDTo, accountTo.getMoney());
     }
 }
